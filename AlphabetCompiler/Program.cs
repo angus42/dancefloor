000e148ec47c45fd2931344ff2221d76bee9bad1
angus42 <Matthias.Greim@live.com>
2015-01-22 19:54:27 +0100

    Added AlphabetCompiler that creates the bit patterns for the TextPlayer out of a sequence.

---
+++ AlphabetCompiler/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace AlphabetCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = "Alphabet.dfs";
+            var headerFilePath = @"..\..\..\Alphabet.h";
+            if (File.Exists(headerFilePath))
+                File.Delete(headerFilePath);
+            using (var headerFile = File.CreateText(headerFilePath))
+            {
+                using (var sequencerFile = File.OpenText(sourceFile))
+                using (var sequencerFileReader = new JsonTextReader(sequencerFile))
+                {
+                    dynamic root = (JObject)JToken.ReadFrom(sequencerFileReader);
+                    headerFile.WriteLine("const PROGMEM byte alphabet[][] = {{");
+                    var firstStep = true;
+                    byte asciiChar = 32 - 1;
+                    foreach (var step in root.steps)
+                    {
+                        // a step equals a letter
+                        asciiChar++;
+                        if (!firstStep)
+                            headerFile.WriteLine(",");
+                        else
+                            firstStep = false;
+                        headerFile.Write("{ // ");
+                        headerFile.Write(Encoding.ASCII.GetChars(new[] { asciiChar }));
+                        headerFile.WriteLine();
+                        var firstStripe = true;
+                        int stripe = 0;
+                        while (true)
+                        {
+                            if (!firstStripe)
+                                headerFile.WriteLine(", ");
+                            else
+                                firstStripe = false;
+                            // we rotate each letter 90° clockwise.
+                            // this gives us vertical stripes of the letters with the LSB beeing the uppermost pixel.
+                            // rows become the bits in the stripe.
+                            for (int b = 7; b >= 0; b--)
+                            {
+                                if (step.frame.Count <= b)
+                                {
+                                    headerFile.Write("0");
+                                }
+                                else
+                                {
+                                    var row = step.frame[b];
+                                    var pixel = row[stripe ];
+                                    var v = pixel.v.ToString();
+                                    var c = ColorTranslator.FromHtml(v);
+                                    headerFile.Write(c.GetBrightness() < 0.5 ? "0" : "1");
+                                }
+                            }
+                            headerFile.Write("b");
+                            stripe++;
+                            if (step.frame[0].Count <= stripe)
+                            {
+                                stripe = -1;
+                                break;
+                            }
+                        }
+                        headerFile.Write("}");
+                    }
+                    headerFile.WriteLine("};");
+                }
+            }
+        }
+    }
+}
